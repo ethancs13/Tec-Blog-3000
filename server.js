d7c8b634f4d6b3d716c778a57864bfc5104a2bc9
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 23:43:24 -0700

    3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3306;
 
 // Creates instance of handlebars with custom helpers
 const hbs = exphbs.create({ helpers });
